bf844d6698ea3202b74d80b86bea162434da82e9
Arthur <arthurkaplan@gmx.at>
2022-04-29 06:34:47 +0200

    Added Unit Tests for Request.java

---
+++ src/request/RequestTest.java
@@ -0,0 +1,42 @@
+package request;
+
+import elevator.Direction;
+import exception.ElevatorException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequestTest {
+
+    Request request = new Request(0, 30, Direction.UP);
+
+    RequestTest() throws ElevatorException {
+    }
+
+    @Test
+    void getCurrentRequestFloor() {
+        assertEquals(0, request.getCurrentRequestFloor());
+    }
+
+    @Test
+    void setCurrentRequestFloor() throws ElevatorException {
+        request.setCurrentRequestFloor(11);
+        assertEquals(11, request.getCurrentRequestFloor());
+    }
+
+    @Test
+    void getDestinationRequestFloor() {
+        assertEquals(30, request.getDestinationRequestFloor());
+    }
+
+    @Test
+    void setDestinationRequestFloor() throws ElevatorException {
+        request.setDestinationRequestFloor(44);
+        assertEquals(44, request.getDestinationRequestFloor());
+    }
+
+    @Test
+    void getDirection() {
+        assertEquals(Direction.UP.toString(), request.getDirection());
+    }
+}
